fe092a5fe7afd14af0e8736973aea178df205088
rav08k <88479305+rav08k@users.noreply.github.com>
2024-10-31 17:25:21 +0530

    Delete src directory

--- src/components/all_utils/elements/DefButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DefButton(props) {
-  return (
-    <button type={props.type} onClick={props.onClick} className={typeof props.extraClass !== "undefined" ? "def_btn " + props.extraClass : props.className} id={props.id}>{props.children}</button>
-  )
-}
-
-export default DefButton
--- src/components/packages_section/RatingStars.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function RatingStars(props) {
-  return (
-    <span className={"star "+ props.extraClass}>★</span>
-  )
-}
-
-export default RatingStars
